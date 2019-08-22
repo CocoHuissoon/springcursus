b00980c3886deb047bf7aa1cfc4a15c87cd46344
coco.huissoon <coco.huissoon@kvk.nl>
2019-08-22 08:38:32 +0200

    first commit

---
+++ src/main/java/coco/springframework/springcursus/SpringcursusApplication.java
@@ -0,0 +1,13 @@
+package coco.springframework.springcursus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcursusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcursusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/coco/springframework/springcursus/SpringcursusApplicationTests.java
@@ -0,0 +1,16 @@
+package coco.springframework.springcursus;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringcursusApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
