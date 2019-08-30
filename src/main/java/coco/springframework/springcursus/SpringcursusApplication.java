67cb52728f386f587b3eed009196d38b3ee29858
coco.huissoon <coco.huissoon@kvk.nl>
2019-08-30 14:26:12 +0200

    next

--- src/main/java/coco/springframework/springcursus/SpringcursusApplication.java
+++ src/main/java/coco/springframework/springcursus/SpringcursusApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringcursusApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringcursusApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringcursusApplication.class, args);
+    }
 
 }
